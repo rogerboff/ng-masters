30a04f8169bb8d9b06f2b664c14d47b741d75430
Roger Pereira Boff <rogerboff@gmail.com>
2015-09-24 19:30:21 -0300

    - Resposta para a questão posição X e Y.

---
+++ resposta/js/base.js
@@ -0,0 +1,42 @@
+//Bizu do stackoverflow
+//http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+	return this.slice(0, str.length) == str;
+  };
+}
+if (typeof String.prototype.endsWith != 'function') {
+  String.prototype.endsWith = function (str){
+	return this.slice(-str.length) == str;
+  };
+}
+
+if(!window.Global){
+    window.Global = {};
+}
+if(!Global.angular_dependencies){
+  Global.angular_dependencies = [];
+}
+
+angular.module("myapp", Global.angular_dependencies);
+console.log('criou o myapp');
+angular.module("myapp").config(function($interpolateProvider){
+	$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
+
+Global.friendly_date = function(d){
+    var delta = new Date().getTime() - d.getTime();
+    var seconds = Math.floor(delta / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+    if(minutes < 1){
+        return seconds + " seconds ago";
+    } else if(hours < 1){
+        return minutes + " minutes ago";
+    } else if(days < 1){
+        return hours + " hours ago";
+    } else {
+        return days + " days ago";
+    }
+}
